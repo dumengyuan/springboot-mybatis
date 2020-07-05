ce99dd0ee03cc0c07a860e07a92e3b591a5c1e4e
杜梦园 <dumengyuanku@163.com>
2020-07-05 19:54:28 +0800

    master AAAA

--- src/main/java/com/atguigu/gittest/GitTest.java
+++ src/main/java/com/atguigu/gittest/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.gittest;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("===>>>> from hotfix");
+        System.out.println("===>>>> from masterAAAA");
     }
 }
